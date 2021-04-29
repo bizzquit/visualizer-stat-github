e886e8029ebf2f792ab65801ae5ca9ec797adb99
Serg <36944165+bizzquit@users.noreply.github.com>
2021-04-29 17:41:06 +0500

    ещё немножечко правок

--- src/components/RepositoryView/Header.tsx
+++ src/components/RepositoryView/Header.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { RepositoryViewProps } from './RepositoryView';
+import { GitForkIcon } from '@primer/octicons-react';
 
 const Header: React.FC<RepositoryViewProps> = ({ data }) => {
   return (
     <div className="p-d-flex p-ai-center">
       <section className="repo-header p-mr-3">
-        {data.fork ? <h1 className="fork">{data.name}</h1> : <h1>{data.name}</h1>}
+        {data.fork ? (
+          <h1 className="fork">
+            {data.name}
+            <GitForkIcon size={24} fill={'red'} />
+          </h1>
+        ) : (
+          <h1>{data.name}</h1>
+        )}
       </section>
       <p>{data.description && `~ ${data.description}`}</p>
     </div>
